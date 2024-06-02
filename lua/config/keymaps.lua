13b9a550c4ce94c246c5becf47038c6bd1792e89
Your Name <your.email@example.com>
2024-06-01 22:54:35 -0300

    Added LSPs and added some plugins. also configurated others

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,6 +2,8 @@ vim.keymap.set("n", "<Esc>", ":", { desc = "Terminal mode with double escape fro
 vim.keymap.set("n", "r", ":redo<CR>", { desc = "Redo" })
 vim.keymap.set("n", "<M-Up>", "ddkP", { desc = "Move line up" })
 vim.keymap.set("n", "<M-Down>", "ddp", { desc = "Move line down" })
+vim.keymap.set("n", "<C-c>", ":wq<CR>", { desc = "Save file and exit Neovim" })
+vim.keymap.set("n", "<C-s>", ":w<CR>", { desc = "Save file" })
 
 -- nvim-tree keymaps
 vim.keymap.set("n", "<C-b>", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle NvimTree" })
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,8 +1,28 @@
-return {
+M = {
 	"williamboman/mason.nvim",
-	opts = {
-		ensure_installed = {
-			"clangd",
-		}
-	}
+	dependencies = {
+    'williamboman/mason-lspconfig.nvim',
+    'nvim-lua/plenary.nvim',
+	},
+	automatic_installation = true,
 }
+
+local servers = {
+	"lua_ls",
+	"clangd",
+}
+
+function M.config()
+	require("mason").setup()
+	require("mason-lspconfig").setup_handlers({
+		function (server_name)
+		    require("lspconfig")[server_name].setup {}
+		end
+	})
+	require("mason-lspconfig").setup({
+      ensure_installed = servers,
+      automatic_installation = true,
+	})
+end
+
+return M
