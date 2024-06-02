255ad0753cefbbab5d18db688e9474a5791a3bfc
Fernando <ferchumatutepessoa471@gmail.com>
2024-06-02 20:09:30 -0300

    Fixed lspconfig

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -4,24 +4,5 @@ return {
 		"williamboman/mason.nvim",
 	},
 	config = function()
-		local lspconfig = require("lspconfig")
-		lspconfig.lua_ls.setup({
-			settings = {
-    	  Lua = {
-    	    runtime = {
-    	      version = "LuaJIT",
-    	    },
-    	    diagnostics = {
-    	      globals = { "vim" },
-    	    },
-    	    workspace = {
-						library = vim.api.nvim_get_runtime_file("", true),
-    	    },
-    	    telemetry = { -- Do not send telemetry data containing a randomized but unique identifier
-    	      enable = false,
-    	    },
-				},
-  		},
-		})
 	end
 }
